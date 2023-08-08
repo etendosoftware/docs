511eb3781eee634f990f55d840f19272df3f4322
MAP English Services <eliana.papes@smfconsulting.es>
2023-08-08 14:29:48 -0300

    Deployed 3e88186 with MkDocs version: 1.4.3

--- custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var collapsibles = document.querySelectorAll(".md-nav__toggle");
-    collapsibles.forEach(function (collapsible) {
-      collapsible.click();
-    });
-  });
-  
