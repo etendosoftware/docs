ac283266bcf77b0f05d62ac7f2287549ee7c91c5
etendobot <noreply@smfconsulting.es>
2025-01-21 17:47:07 +0000

    Deployed 50bca1de2 to 24.x with MkDocs 1.6.1 and mike 2.1.3

---
+++ 24.x/assets/javascripts/algolia.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var metaTag = document.createElement('meta');
+    metaTag.name = "algolia-site-verification";
+    metaTag.content = "811431398E4820CE";
+    document.head.appendChild(metaTag);
+});
+
