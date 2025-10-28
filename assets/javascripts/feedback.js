6a5b1932e6109264308988caf4a0ea2f0f930232
etendobot <noreply@smfconsulting.es>
2025-10-28 17:44:29 +0000

    Deployed f8923ae7e with MkDocs version: 1.6.1

--- assets/javascripts/feedback.js
+++ assets/javascripts/feedback.js
@@ -1,17 +1,28 @@
-function updateFeedbackLink() {
-    const feedbackLink = document.getElementById("feedback-link");
+function updateFeedbackLink(elementId, formId) {
+    const feedbackLink = document.getElementById(elementId);
     if (feedbackLink) {
-    const currentPath = window.location.pathname;
-    const formUrl = `https://docs.google.com/forms/d/15xgzQaUTdaxS6Z_jM0h4zYP4ebALOWmDz__pG2JLFb0/viewform?entry.1159911930=${encodeURIComponent(currentPath)}`;
-    feedbackLink.href = formUrl;
+        const currentPath = window.location.pathname;
+        const formUrl = `https://docs.google.com/forms/d/${formId}/viewform?entry.1159911930=${encodeURIComponent(currentPath)}`;
+        feedbackLink.href = formUrl;
     }
 }
 
-document.addEventListener("DOMContentLoaded", updateFeedbackLink);
+function updateNegativeFeedbackLink() {
+    updateFeedbackLink("n-feedback-link", "15xgzQaUTdaxS6Z_jM0h4zYP4ebALOWmDz__pG2JLFb0");
+}
+
+function updatePositiveFeedbackLink() {
+    updateFeedbackLink("p-feedback-link", "1agexla60x2li2t5OZf9CV2Jd7P_B9Ecc1d2VK7lUNLw");
+}
+
+document.addEventListener("DOMContentLoaded", updateNegativeFeedbackLink);
+document.addEventListener("DOMContentLoaded", updatePositiveFeedbackLink);
 
 const observer = new MutationObserver(() => {
-updateFeedbackLink();
+updateNegativeFeedbackLink();
+updatePositiveFeedbackLink();
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
-window.addEventListener("popstate", updateFeedbackLink);
+window.addEventListener("popstate", updateNegativeFeedbackLink);
+window.addEventListener("popstate", updatePositiveFeedbackLink);
